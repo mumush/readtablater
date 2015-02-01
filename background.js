ab0ccfdc96035ca1701b30512259b4b0a4ecb8c6
mumush <rhoff37@gmail.com>
2015-01-31 19:21:26 -0500

    Add and Remove pages for later
    + Add pages to the newtab page
    + Remove pages from the newtab page
    + Styled the newtab page (not final)

--- background.js
+++ background.js
@@ -1,41 +1,80 @@
 // background.js
 //
-//
 
+
+//Array holding dictionaries of pages the user wants to read/view later
+//Each elements dictionary has the following keys: {"url", "title", "screenshotData"}
 var savedForLaterPages = [];
 
+//When the browser action button has been clicked...
 chrome.browserAction.onClicked.addListener(function(tab) {
 
-	//first check if the tab url has already been saved for later
+	//First check if the tabs url has already been saved
 	var isNewPage = isNewPageToSave(tab.url);
 
-	if (isNewPage) {
+	if (isNewPage) { //Page is new, and should be saved to the array
 
-		alert("Page is new!");
-
+		//Take a screenshot of the current tab
 		chrome.tabs.captureVisibleTab(null, {}, function(dataUrl) {
+			//Add a new dictionary entry to the savedForLaterPages array
 			savedForLaterPages.push({"url": tab.url, "title": tab.title, "screenshotData": dataUrl});
 		});
 	}
 	else {
 
-		alert("Page already saved!");
+		//alert("Page already saved!");
 	}
 
 
 });
 
-
+//Listen for messages from index.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
+	//index.js has requested the array of page dictionaries
     if (request.getPagesToDisplay == true) {
+
       sendResponse({pagesToDisplay: savedForLaterPages});
+
+    }
+    //index.js has requested to remove a page from the savedForLaterPages array
+    else if (request.removePage == true) {
+
+    	//Get the url sent with the request and remove it
+    	removePage(request.urlToRemove);
+
     }
 
 });
 
 
+//Removes the page dictionary from savedForLaterPages that has a url matching the "urlToRemove"
+function removePage(urlToRemove) {
 
+	//Get the index of the page that with a url === urlToRemove
+
+	var indexOfUrlInArray;
+
+	for( index = 0; index < savedForLaterPages.length; index++ ) {
+		if( savedForLaterPages[index].url === urlToRemove ) {
+			indexOfUrlInArray = index;
+		}
+	}
+
+	alert(savedForLaterPages);
+
+	//Remove the page dictionary at indexOfUrlInArray
+	savedForLaterPages.splice(indexOfUrlInArray, 1);
+
+	alert(savedForLaterPages.toString());
+
+	//Alert index.js that the page has been removed from the array
+	sendResponse({pageRemoved: true});
+
+}
+
+
+//Determines if the url passed in has already been saved for later
 function isNewPageToSave(tabUrl) {
 
 	for (index = 0; index < savedForLaterPages.length; index++) { 
@@ -48,3 +87,6 @@ function isNewPageToSave(tabUrl) {
 
 	return true;
 }
+
+
+
--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 // index.js
 //
 
+//When the newtab page is first opened, tell background.js that we want all of the urls the user has saved for later
 chrome.runtime.sendMessage({getPagesToDisplay: true}, function(response) {
 
-		//alert(response.pagesToDisplay.length);
-
 		addPageImages(response.pagesToDisplay);
-
 });
 
 
+//Receives an array of pages
+//Each element of the array contains a dictionary with the following keys: {"url", "title", "screenshotData"}
+//Creates an HTML <figure> for each page in the array
 function addPageImages(pagesToDisplay) {
 
 	for (index = 0; index < pagesToDisplay.length; index++) { 
 		
 		//alert("Index: " + index + "~ Title: " + pagesToDisplay[index].title);
 		//alert("Screenshot Data: " + pagesToDisplay[index].screenshotData);
-		createUrlImageLink(pagesToDisplay[index]);
-
+		createUrlFigure(pagesToDisplay[index]);
 	}
 
 }
 
-//creates an img tag
-function createUrlImageLink(page) {
+//Creates a figure tag containing an image, link, button, and figcaption
+//	<figure>
+//		<button></button>
+//		<a><img></a>
+//		<figcaption></figcaption>
+//	</figure>
+function createUrlFigure(page) {
 
 	//Create a <figure> tag
 	var pagePreviewFigure = document.createElement("figure");
@@ -51,6 +56,16 @@ function createUrlImageLink(page) {
 	pagePreviewLink.appendChild(pageImageElement);
 
 	//Insert the a and figcaption elements into the figure element --> <figure><a></a><figcaption></figcaption></figure>
+	
+	var pagePreviewRemoveBut = document.createElement("button");
+	pagePreviewRemoveBut.innerHTML = "&#10005;";
+	pagePreviewRemoveBut.className = "pagePreviewRemoveBut";
+	pagePreviewRemoveBut.setAttribute("title", "Remove page");
+	//add an event listener to react when the button has been clicked
+	pagePreviewRemoveBut.addEventListener('click', removePage);
+
+	pagePreviewFigure.appendChild(pagePreviewRemoveBut);
+
 	pagePreviewFigure.appendChild(pagePreviewLink);
 	pagePreviewFigure.appendChild(pagePreviewFigCaption)
 
@@ -58,3 +73,41 @@ function createUrlImageLink(page) {
 	document.getElementById("pagesForLater").appendChild(pagePreviewFigure);
 
 }
+
+
+//Called when a "remove" button has been clicked
+function removePage(event) {
+
+	var clickedRemoveBut = event.currentTarget
+	//get the clicked buttons parent, the parent figure element
+	var parentFigure = clickedRemoveBut.parentNode;
+
+	//grab the href of the child <a> tag inside the figure element
+	//passed along in message to background.js
+	var link = parentFigure.getElementsByClassName("pagePreviewLink")[0];
+	var linkUrl = link.getAttribute("href");
+	alert("URL to Remove: " + linkUrl);
+
+	//Remove each of the children inside the parent <figure> element
+	var firstChildOfFigure = parentFigure.firstChild;
+
+	while(firstChildOfFigure) {
+
+		parentFigure.removeChild(firstChildOfFigure);
+
+		firstChildOfFigure = parentFigure.firstChild;
+	}
+
+	//Now remove the parent figure element
+	parentFigure.remove();
+
+	//Send a message to background.js letting know that we have removed a page
+	//and want to remove the url from the backend array in background.js
+	chrome.runtime.sendMessage({removePage: true, urlToRemove: linkUrl}, function(response) {
+
+		alert("Page Removed: " + response.pageRemoved);
+	});
+
+}
+
+
